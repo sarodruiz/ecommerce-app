6145480dde8830e738541b32a6ec89c8ff393657
sarodruiz <sarodruiz@gmail.com>
2025-02-08 00:33:05 -0300

    added connection to database code to server.tx

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,16 @@
 import express, { Express } from "express";
+import mongoose from "mongoose";
 
 const app: Express = express();
-const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+const PORT = Number(process.env.PORT) || 5000;
+const MONGODB_URI = process.env.MONGODB_URI || "";
+
+mongoose.connect(MONGODB_URI)
+    .then(() => {
+        console.log("Connected to MongoDB");
+        app.listen(PORT, () => {
+            console.log(`Server running on http://localhost:${PORT}`);
+        });
+    })
+    .catch(error => console.log("Error connecting to MongoDB", error));
