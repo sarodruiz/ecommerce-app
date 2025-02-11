78aeae9b7589caf658e512f0f50c2308bcf2c022
sarodruiz <sarodruiz@gmail.com>
2025-02-11 15:31:42 -0300

    created /api/users/login route

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -4,5 +4,8 @@ import { registerUser } from "../controllers/user.controller";
 const router = Router();
 
 router.post("/register", registerUser);
+router.post("/login", (req, res) => {
+    res.status(200).json({ message: "Welcome to login route" });
+});
 
 export default router;
